fc789ee7960a0aa4e7435d50f8c30b3a0200c639
roleecorn <roleecorn@gmail.com>
2022-12-23 17:08:43 +0800

    new_html_template

--- main.py
+++ main.py
@@ -2,29 +2,32 @@ from typing import Optional
 import requests
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
+from fastapi.staticfiles import StaticFiles
 app = FastAPI()  # 建立一個 Fast API application
-
+app.mount("/html5up-phantom", StaticFiles(directory="html5up-phantom"), name="html5up-phantom")
 
 @app.get("/")  # 指定 api 路徑 (get方法)
 def read_root():
+    f = open("home.html", mode="r", encoding="utf8").read()
     ht = """
     <html>
         <head>
             <title>首頁</title>
         </head>
         <body>
-            <input type=\"button\" value=\"連結名稱\" onclick=\"location.href=\'http://localhost:8000/test\'\">
+            <input type=\"button\" value=\"資料展示\" onclick=\"location.href=\'http://localhost:8000/test\'\">
+            <input type=\"button\" value=\"模板展示\" onclick=\"location.href=\'http://localhost:8000/html5up-phantom/index.html'\">
         </body>
     <html>"""
-    return HTMLResponse(content=ht, status_code=200)
+    return HTMLResponse(content=f, status_code=200)
 
 
 @app.get("/test")
 def showwaterfall(data: str = ""):
     f = open("img.html", mode="r", encoding="utf8").read()
     script_lines = f.split("\n")
-    template = "url: '/data?dbs={}',"
-    script_lines[61] = template.format(data)
+    template = f"url: '/data?dbs={data}',"
+    # script_lines[61] = template
     script_content = "\n".join(script_lines)
 
     return HTMLResponse(content=script_content, status_code=200)
