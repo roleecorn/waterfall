c8b9d204826b771094d9d68ee171e28512a7fb90
Sun-I-Fu <jeff77122000@gmail.com>
2023-03-14 22:22:04 +0800

    rename

--- main.py
+++ main.py
@@ -16,15 +16,15 @@ def read_root():
     return HTMLResponse(content=f, status_code=200)
 
 
-@app.get("/test")
-def showwaterfall(data: str = ""):
+@app.get("/chart")
+def showchart(data: str = ""):
     f = open("chart.html", mode="r", encoding="utf8").read()
 
     return HTMLResponse(content=f, status_code=200)
 
 
-@app.get("/test2")
-def showchart(data: str = ""):
+@app.get("/waterfall")
+def showwaterfall(data: str = ""):
     f = open("img2.html", mode="r", encoding="utf8").read()
 
     return HTMLResponse(content=f, status_code=200)
@@ -72,53 +72,6 @@ def imgs(dbs: str = "", shop: str = "eddiebauer", name: str = ""):
     }
     return JSONResponse(ret)
 
-
-@app.get("/chart")
-def chart():
-    data = [12, 19, 3, 1]
-    labels = ["Red", "Green", "Blue", "test"]
-    colors = [
-        "#1f77b4",
-        "#ff7f0e",
-        "#2ca02c",
-        "#d62728",
-        "#9467bd",
-        "#8c564b",
-        "#e377c2",
-        "#7f7f7f",
-        "#bcbd22",
-        "#17becf"
-    ]
-    color = colors[:len(data)]
-    chart_ = {
-        'labels': labels,
-        'datasetLabel': "mychart",
-        'data': data,
-        'backgroundColor': color,
-        'borderWidth': 1
-    }
-    ret = {
-        'status': True,
-        'data': chart_
-    }
-    return JSONResponse(ret)
-
-
-@app.get("/count_test")
-def count_test(search: str):
-    conn = sqlite3.connect("eddiebauer.db")
-    cursor = conn.cursor()
-
-    query = f"SELECT COUNT(*) FROM eddiebauer WHERE name LIKE '%{search}%'"
-    cursor.execute(query)
-    result = cursor.fetchone()
-    conn.close()
-    ret = {
-        'status': True,
-        'data': result
-    }
-
-    return JSONResponse(ret)
 
 
 @app.post("/search")
