a3cb1192bf7cdc8c535b37407925afaca1844841
roleecorn <roleecorn@gmail.com>
2023-04-17 20:19:39 +0800

    chart_addver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 import sqlite3
 import pandas as pd
@@ -9,6 +9,7 @@ app.mount("/html5upphantom",
           StaticFiles(directory="html5upphantom"), name="html5upphantom")
 app.mount("/assets",
           StaticFiles(directory="assets"), name="assets")
+
 
 @app.get("/")  # 指定 api 路徑 (get方法)
 def read_root():
@@ -36,7 +37,7 @@ def dbs():
     for company in os.listdir("html5upphantom\images"):
         if "." in company:
             continue
-        files[company]={"name": company, "src": company}
+        files[company] = {"name": company, "src": company}
     ret = {
         'status': True,
         'data': files
@@ -83,11 +84,9 @@ def imgs(dbs: str = "", shop: str = "eddiebauer",
     return JSONResponse(ret)
 
 
-
 @app.post("/search")
 async def search(data: dict):
     print(data)
-    # return
     colors = [
         "#1f77b4",
         "#ff7f0e",
@@ -107,31 +106,53 @@ async def search(data: dict):
         search_term = data['inputs'][i]
         conn = sqlite3.connect(f"{company}.db")
         cursor = conn.cursor()
-        query = f"SELECT COUNT(*) FROM {company} WHERE price < {search_term}"
+        find_max_ver = f'''
+        SELECT MAX(ver)
+        FROM {company}
+        '''
+        cursor.execute(find_max_ver)
+        ver = cursor.fetchone()[0]
+        cursor.close
+        query = f"SELECT COUNT(*) FROM {company} WHERE price < {search_term} AND ver = {ver}"
         if search_type == "cost":
-            query = f"SELECT COUNT(*) FROM {company} WHERE price < {search_term}"
+            query = f'''SELECT COUNT(*) FROM {company}
+                        WHERE price < {search_term}
+                        AND ver = {ver}'''
         elif search_type == "feature":
-            query = f"SELECT COUNT(*) FROM {company} WHERE path LIKE '%{search_term}%'"
+            query = f'''SELECT COUNT(*) FROM {company}
+                        WHERE path LIKE '%{search_term}%'
+                        AND ver = {ver}'''
         elif search_type == "name":
-            query = f"SELECT COUNT(*) FROM {company} WHERE name LIKE '%{search_term}%'"
+            query = f'''SELECT COUNT(*) FROM {company} 
+                        WHERE name LIKE '%{search_term}%'
+                        AND ver = {ver}'''
         else:
             return
+        # if search_type == "cost":
+        #     query = f"SELECT COUNT(*) FROM {company} WHERE price < {search_term} AND ver = {ver}"
+        # elif search_type == "feature":
+        #     query = f"SELECT COUNT(*) FROM {company} WHERE path LIKE '%{search_term}%' AND ver = {ver}"
+        # elif search_type == "name":
+        #     query = f"SELECT COUNT(*) FROM {company} WHERE name LIKE '%{search_term}%' AND ver = {ver}"
+        # else:
+        #     return
+        cursor = conn.cursor()
         cursor.execute(query)
         search_res.append(cursor.fetchone()[0])
         cursor.close()
         conn.close()
     color = colors[:len(data['inputs'])]
     for i in range(len(data['inputs'])):
-        data['searchCompanys'][i]+='：'
-        data['searchCompanys'][i]+=str(data['inputs'][i])
+        data['searchCompanys'][i] += '：'
+        data['searchCompanys'][i] += str(data['inputs'][i])
     chart_ = {
         'labels': data['searchCompanys'],
         'datasetLabel': search_type,
         'data': search_res,
         'backgroundColor': color,
         'borderWidth': 1,
-        'scales_min' : 0,
-        'scales_max' :max(search_res)
+        'scales_min': 0,
+        'scales_max': max(search_res)
     }
     ret = {
         'status': True,
